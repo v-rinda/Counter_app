e350b395628a8e553a3c2c13ed373e7983fd8645
v-rinda <107861453+v-rinda@users.noreply.github.com>
2023-08-10 17:41:54 +0530

    Commiting counter app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+    const [count,setCount]=useState(0);
+    function minus(){
+      setCount(count-1);
+    }
+    function plus()
+    {
+      setCount(count+1);
+    }
+    function reset()
+    {
+      setCount(0);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+       <div className="text-[#0398d4] font-medium text-2xl"> Increment & Decrement</div>
+      
+       <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151] ">
+           <button  onClick={minus} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl"> - </button>
+           <div className="font-bold gap-12 text-5xl" > {count} </div>
+           <button onClick={plus} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl"> + </button> 
+       </div>
+
+      <button onClick={reset} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg"> Reset </button> 
+   </div>
   );
 }
 
